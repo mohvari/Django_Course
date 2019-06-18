3c07ce0e506819270b7e3253dc6e3fe4ea4cdba4
mohvari <zavvari@ce.sharif.edu>
2019-06-18 22:05:37 +0430

    Last commit was about Filter not Tag! Add templatetag

--- base/templatetags/basetags.py
+++ base/templatetags/basetags.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
 from django import template
 
 register = template.Library()
 @register.filter()
 def price_format(value):
     return "{}$".format(value)
+
+@register.simple_tag()
+def get_time(string_format):
+    return datetime.now().strftime(string_format)
